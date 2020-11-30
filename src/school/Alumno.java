22af98ab07a89b8c54fda3eb79694a2f84112a1e
Nestorsoiza <nestor.soiza83@gmail.com>
2020-11-30 23:35:51 +0100

    Alumno aplico
    me da pegas en el toString,

--- src/school/Alumno.java
+++ src/school/Alumno.java
@@ -1,5 +1,38 @@
 package school;
 
 public class Alumno extends Persona {
-	private
+	private int nota;
+
+	public Alumno() {
+		super();
+		nota = MetodosSueltos.generaNumeroAleatorio(0, 10);
+		super.setEdad(MetodosSueltos.generaNumeroAleatorio(12, 15));
+	}
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	@Override
+	protected void disponibilidad() {
+		int prob = MetodosSueltos.generaNumeroAleatorio(0, 100);
+		if (prob < 50) {
+			super.setAsistencia(false);
+		} else {
+			super.setAsistencia(true);
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre: "  +   super.getNombre()
+		+ ", Sexo: " + super.getSexo() + ", nota: " + nota;
+	}
+	
+	
 }
