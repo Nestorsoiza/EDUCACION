67432eed7b37bbe02df80b79c7b0a2e573992ce4
Nestorsoiza <nestor.soiza83@gmail.com>
2020-12-01 21:54:33 +0100

    Clase profesor hecha
    me da pegas la cte MATERIAS

--- src/school/Profesor.java
+++ src/school/Profesor.java
@@ -2,9 +2,33 @@ package school;
 
 public class Profesor extends Persona {
 
+	private String materia;
+	
+	
+	public Profesor() {
+		super();
+		super.setEdad(MetodosSueltos.generaNumeroAleatorio(25, 50));
+		materia = Constantes.MATERIAS[MetodosSueltos.generaNumeroAleatorio(0, 2)];
+	}
+
+
+	public String getMateria() {
+		return materia;
+	}
+
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+
 	@Override
 	protected void disponibilidad() {
-		// TODO Auto-generated method stub
-		
+		int prob = MetodosSueltos.generaNumeroAleatorio(0, 100);
+		if (prob < 20) {
+			super.setAsistencia(false);
+		} else {
+			super.setAsistencia(true);
+		}
 	}
 }
